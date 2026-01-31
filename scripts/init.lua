00f02d251a77e8441ceb04527891a5ae834a84c9
Apheayenne <Zelda,ganondorf@gmail.com>
2026-01-31 01:34:05 -0500

    [Feature]: Initial Commit for Cat Quest PopTracker

---
+++ scripts/init.lua
@@ -0,0 +1,13 @@
+ENABLE_DEBUG_LOG = true
+-- Maps
+Tracker:AddMaps("maps/map.json")
+
+-- Layouts
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/item_grid.json")
+
+-- Locations
+Tracker:AddLocations("locations/locations.json")
+
+-- Items
+Tracker:AddItems("items/items.json")
