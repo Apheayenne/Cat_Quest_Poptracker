a3b4c67c22b8759422d833ed309d7d68f0e9b0f1
Apheayenne <Zelda,ganondorf@gmail.com>
2026-02-03 00:13:07 -0500

    [Feature] AutoTracking fixes

--- scripts/autotracking.lua
+++ scripts/autotracking.lua
@@ -1,11 +1,12 @@
 -- Configuration --------------------------------------
 AUTOTRACKER_ENABLE_DEBUG_LOGGING = true or ENABLE_DEBUG_LOG
+AUTOTRACKER_ENABLE_DEBUG_LOGGING_AP = true
 AUTOTRACKER_ENABLE_ITEM_TRACKING = true
 AUTOTRACKER_ENABLE_LOCATION_TRACKING = true
 -------------------------------------------------------
 
-ScriptHost:LoadScript("scripts/item_mapping.lua")
-ScriptHost:LoadScript("scripts/location_mapping.lua")
+ScriptHost:LoadScript("scripts/item_mappings.lua")
+ScriptHost:LoadScript("scripts/location_mappings.lua")
 
 CUR_INDEX = -1
 SLOT_DATA = nil
@@ -51,26 +52,29 @@ function Dump_table(o, depth)
 end
 
 function OnClear(slot_data)
-    DebugAP(string.format("called onClear, slot_data:\n%s", Dump_table(slot_data)))
+    DebugAP(string.format("called onClear, slot_data:\n<%s>", Dump_table(slot_data)))
     print(Dump_table(SLOT_DATA))
     SLOT_DATA = slot_data
     CUR_INDEX = -1
     
     for _, v in pairs(LOCATION_MAPPINGS) do
         if v[1] then
-            local obj = Tracker:GetObjectForCode(v[1])
+            DebugAP(string.format("onClear: clearing location '<%s>'", v[1]))
+            local obj = Tracker:FindObjectForCode(v[1])
             if obj then
                 if (v[1]:sub(1,1) == "@") then
                     obj.AvailableChestCount = obj.ChestCount
                 end
+            else 
+                DebugAP(string.format("onClear: could not find object for code '<%s>'", v[1]))
             end
         end
     end
 
     for _, v in pairs(ITEM_MAPPINGS) do
         if v[1] and v[2]then
-            DebugAP(string.format("onClear: clearing item '%s' of type '%s'", v[1], v[2]))
-            local obj = Tracker:GetObjectForCode(v[1])
+            DebugAP(string.format("onClear: clearing item '<%s>' of type '<%s>'", v[1], v[2]))
+            local obj = Tracker:FindObjectForCode(v[1])
             if obj then
                 if v[2] == "toggle" then
                     obj.Active = false
@@ -80,10 +84,10 @@ function OnClear(slot_data)
                 elseif v[2] == "consumable" then
                     obj.AcquiredCount = 0
                 elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-                    print(string.format("onClear: unknown item type %s for code %s", v[2], v[1]))
+                    print(string.format("onClear: unknown item type <%s> for code <%s>", v[2], v[1]))
                 end
             elseif AUTOTRACKER_ENABLE_DEBUG_LOGGING then
-                print(string.format("onClear: could not find object for code %s", v[1]))
+                print(string.format("onClear: could not find object for code <%s>", v[1]))
             end
         end
     end
@@ -98,20 +102,22 @@ function OnItem(index, item_id, item_name, player_number)
 		return
 	end
 	
+    DebugAP(string.format("OnItem called with index <%s>, item_id <%s>, item_name <%s>, player_number <%s>", index, item_id, item_name, player_number))
+
     CUR_INDEX = index;
-	local v = ITEM_MAPPING[item_id]
+	local v = ITEM_MAPPINGS[item_id]
 
     if not v then
-        DebugAP(string.format("onItem: could not find item mapping for id %s", item_id))
+        DebugAP(string.format("onItem: could not find item mapping for id <%s>", item_id))
         return
     end
-    DebugAP(string.format("onItem: code: %s, type %s", v[1], v[2]))
+    DebugAP(string.format("onItem: code: <%s>, type <%s>", v[1], v[2]))
 
     if not v[1] then
 		return
 	end
 
-    local obj = Tracker:GetObjectForCode(v[1])
+    local obj = Tracker:FindObjectForCode(v[1])
     if v[2] == "toggle" then
         obj.Active = true
     elseif v[2] == "progressive" then
@@ -120,34 +126,45 @@ function OnItem(index, item_id, item_name, player_number)
     elseif v[2] == "consumable" then
         obj.AcquiredCount = obj.AcquiredCount + 1
     else
-        DebugAP(string.format("onItem: unknown item type %s for code %s", v[2], v[1]))
+        DebugAP(string.format("onItem: unknown item type <%s> for code <%s>", v[2], v[1]))
     end
 end
 
 function OnLocation(index, location_id, location_name)
+    DebugAP("")
     if not AUTOTRACKER_ENABLE_LOCATION_TRACKING then
-        return
-    end
+		return
+	end
+    
+    DebugAP(string.format("OnLocation called with index <%s>, location_id <%s>, location_name <%s>", index, location_id, location_name))
 
     CUR_INDEX = index;
-    local v = LOCATION_MAPPING[location_id]
+    local v = LOCATION_MAPPINGS[index]
+
+    DebugAP(string.format("onLocation: value <%s>", v[1]))
 
     if not v then
-        DebugAP(string.format("onLocation: could not find location mapping for id %s", location_id))
+        DebugAP(string.format("onLocation: could not find location mapping for id <%s>", location_id))
         return
     end
 
     if not v[1] then
-        DebugAP(string.format("onLocation: no code for location id %s", location_id))
+        DebugAP(string.format("onLocation: no code for location id <%s>", location_id))
         return
     end
 
-    local obj = Tracker:GetObjectForCode(v[1])
+    local obj = Tracker:FindObjectForCode(v[1])
+    --DebugAP(string.format("onLocation: found object <%s> for code <%s>", tostring(obj), v[1]))
     if obj then
-        obj.Active = true
-        DebugAP(string.format("onLocation: activated object for code %s", v[1]))
+        if v[1]:sub(1, 1) == "@" then
+            --DebugAP(string.format("onLocation: activated object for code <%s>", v[1]))
+			obj.AvailableChestCount = obj.AvailableChestCount - 1
+            --DebugAP(string.format("Total Chest Count <%s> available <%s>", obj.ChestCount, obj.AvailableChestCount))
+		else
+			obj.Active = true
+		end
     else
-        DebugAP(string.format("onLocation: could not find object for code %s", v[1]))
+        --DebugAP(string.format("onLocation: could not find object for code <%s>", v[1]))
     end
 end
 
--- scripts/location_mappings.lua
+++ scripts/location_mappings.lua
@@ -3,40 +3,40 @@ BASE = 123500
 LOCATION_MAPPINGS = {
     -- Mewtown
     [BASE + 1]  = {"@Overworld/Mewtown/A Whole New World"},
-    [BASE + 40] = {"@Overworld/Mewtown/The Growling Peasant"},
-    [BASE + 41] = {"@Overworld/Mewtown/The Catnip Ritual"},
-    [BASE + 42] = {"@Overworld/Mewtown/The Circle"},
-    [BASE + 43] = {"@Overworld/Mewtown/Fur-reedom!"},
-    [BASE + 44] = {"@Overworld/Mewtown/The Whispurrer"},
+    [BASE + 40] = {"@Overworld/Mewtown/I. The Growling Peasant"},
+    [BASE + 41] = {"@Overworld/Mewtown/II. The Catnip Ritual"},
+    [BASE + 42] = {"@Overworld/Mewtown/III. The Circle"},
+    [BASE + 43] = {"@Overworld/Mewtown/IV. Fur-reedom!"},
+    [BASE + 44] = {"@Overworld/Mewtown/V. The Whispurrer"},
 
     -- Catpital - West
-    [BASE + 2]  = {"@Overworld/Catpital - West/The Catpital 1"},
-    [BASE + 28] = {"@Overworld/Catpital - West/The King's Mage"},
-    [BASE + 3]  = {"@Overworld/Catpital - West/The Catpital 2"},
-    [BASE + 29] = {"@Overworld/Catpital - West/The Mage Search"},
-    [BASE + 30] = {"@Overworld/Catpital - West/The Spirits"},
-    [BASE + 31] = {"@Overworld/Catpital - West/The Lion King"},
+    [BASE + 2]  = {"@Overworld/Catpital - West/The Catpital (1)"},
+    [BASE + 28] = {"@Overworld/Catpital - West/I. The King's Mage"},
+    [BASE + 3]  = {"@Overworld/Catpital - West/The Catpital (2)"},
+    [BASE + 29] = {"@Overworld/Catpital - West/II. The Mage Search"},
+    [BASE + 30] = {"@Overworld/Catpital - West/III. The Spirits"},
+    [BASE + 31] = {"@Overworld/Catpital - West/IV. The Lion King"},
 
     -- Catpital - East
-    [BASE + 26] = {"@Overworld/Catpital - East/The Missing Soldiers"},
-    [BASE + 27] = {"@Overworld/Catpital - East/The Rescue"},
+    [BASE + 26] = {"@Overworld/Catpital - East/I. The Missing Soldiers"},
+    [BASE + 27] = {"@Overworld/Catpital - East/II. The Rescue"},
     [BASE + 65] = {"@Overworld/Catpital - East/The Dragon Worshippers"},
     [BASE + 66] = {"@Overworld/Catpital - East/The Heirloom Armor"},
 
     -- Catpital Lake
-    [BASE + 61] = {"@Overworld/Catpital Lake/Blacksmith Assistance"},
+    [BASE + 61] = {"@Overworld/Catpital Lake/I. Blacksmith Assistance"},
     [BASE + 4]  = {"@Overworld/Catpital Lake/The Dragon and the Cat"},
-    [BASE + 62] = {"@Overworld/Catpital Lake/Blacksmith Apprentice"},
-    [BASE + 63] = {"@Overworld/Catpital Lake/Blacksmith Journeyman"},
-    [BASE + 64] = {"@Overworld/Catpital Lake/Blacksmith Master"},
+    [BASE + 62] = {"@Overworld/Catpital Lake/II. Blacksmith Apprentice"},
+    [BASE + 63] = {"@Overworld/Catpital Lake/III. Blacksmith Journeyman"},
+    [BASE + 64] = {"@Overworld/Catpital Lake/IV. Blacksmith Master"},
     [BASE + 69] = {"@Overworld/Catpital Lake/The Ultimate Dragonsbane"},
     [BASE + 10] = {"@Overworld/Catpital Lake/The Dragonsbane"},
 
     -- Fishercat's Friend
-    [BASE + 36] = {"@Overworld/Fishercat's Friend/Meat Meat Meat!"},
-    [BASE + 37] = {"@Overworld/Fishercat's Friend/Meatmeatmeatmeat!!"},
-    [BASE + 38] = {"@Overworld/Fishercat's Friend/Meat Disposal Crew"},
-    [BASE + 39] = {"@Overworld/Fishercat's Friend/The Meat Seller"},
+    [BASE + 36] = {"@Overworld/Fishercat's Friend/I. Meat Meat Meat!"},
+    [BASE + 37] = {"@Overworld/Fishercat's Friend/II. Meatmeatmeatmeat!!"},
+    [BASE + 38] = {"@Overworld/Fishercat's Friend/III. Meat Disposal Crew"},
+    [BASE + 39] = {"@Overworld/Fishercat's Friend/IV. The Meat Seller"},
     [BASE + 22] = {"@Overworld/Fishercat's Friend/The Second Day of Kitmas"},
     
     -- Queen's Resort
@@ -45,10 +45,10 @@ LOCATION_MAPPINGS = {
     [BASE + 25] = {"@Overworld/Queen's Resort/Mewry Kitmas!"},
     
     -- Bermewda
-    [BASE + 13] = {"@Overworld/Bermewda/The Strange Charm"},
-    [BASE + 14] = {"@Overworld/Bermewda/The Escape"},
-    [BASE + 15] = {"@Overworld/Bermewda/The Sacrifice"},
-    [BASE + 16] = {"@Overworld/Bermewda/The Sanctuary"},
+    [BASE + 13] = {"@Overworld/Bermewda/I. The Strange Charm"},
+    [BASE + 14] = {"@Overworld/Bermewda/II. The Escape"},
+    [BASE + 15] = {"@Overworld/Bermewda/III. The Sacrifice"},
+    [BASE + 16] = {"@Overworld/Bermewda/IV. The Sanctuary"},
     
     -- Founders Island
     [BASE + 68] = {"@Overworld/Founders Island/The Golden Key"},
@@ -57,30 +57,30 @@ LOCATION_MAPPINGS = {
     [BASE + 9]  = {"@Overworld/South of Queens Resort/The Dragonblood"},
     
     -- Cat Grove
-    [BASE + 17] = {"@Overworld/Cat Grove/The Servant of God"},
-    [BASE + 18] = {"@Overworld/Cat Grove/The Offerings"},
+    [BASE + 17] = {"@Overworld/Cat Grove/I. The Servant of God"},
+    [BASE + 18] = {"@Overworld/Cat Grove/II. The Offerings"},
     [BASE + 23] = {"@Overworld/Cat Grove/The Third Day of Kitmas"},
-    [BASE + 19] = {"@Overworld/Cat Grove/The Prayer"},
-    [BASE + 20] = {"@Overworld/Cat Grove/The Godcat, Mauth"},
+    [BASE + 19] = {"@Overworld/Cat Grove/III. The Prayer"},
+    [BASE + 20] = {"@Overworld/Cat Grove/IV. The Godcat, Mauth"},
     
     -- West Pawt
-    [BASE + 49] = {"@Overworld/West Pawt/The West Investigation"},
-    [BASE + 50] = {"@Overworld/West Pawt/The West Heist"},
-    [BASE + 51] = {"@Overworld/West Pawt/The Magic Lock"},
-    [BASE + 52] = {"@Overworld/West Pawt/The Revelation"},
+    [BASE + 49] = {"@Overworld/West Pawt/I. The West Investigation"},
+    [BASE + 50] = {"@Overworld/West Pawt/II. The West Heist"},
+    [BASE + 51] = {"@Overworld/West Pawt/III. The Magic Lock"},
+    [BASE + 52] = {"@Overworld/West Pawt/IV. The Revelation"},
     [BASE + 24] = {"@Overworld/West Pawt/The Fourth Day of Kitmas"},
     
     -- Pawt City
-    [BASE + 45] = {"@Overworld/Pawt City/Path to Water Walking"},
-    [BASE + 46] = {"@Overworld/Pawt City/The Fusion"},
-    [BASE + 47] = {"@Overworld/Pawt City/Rogue Mages"},
-    [BASE + 48] = {"@Overworld/Pawt City/The Miracle"},
+    [BASE + 45] = {"@Overworld/Pawt City/I. Path to Water Walking"},
+    [BASE + 46] = {"@Overworld/Pawt City/II. The Fusion"},
+    [BASE + 47] = {"@Overworld/Pawt City/III. Rogue Mages"},
+    [BASE + 48] = {"@Overworld/Pawt City/IV. The Miracle"},
     
     -- Winter's Edge
-    [BASE + 57] = {"@Overworld/Winter's Edge/The Missing Pages"},
-    [BASE + 58] = {"@Overworld/Winter's Edge/The Protected Pages"},
-    [BASE + 59] = {"@Overworld/Winter's Edge/The Runaway Archeologist"},
-    [BASE + 60] = {"@Overworld/Winter's Edge/The Treasure"},
+    [BASE + 57] = {"@Overworld/Winter's Edge/I. The Missing Pages"},
+    [BASE + 58] = {"@Overworld/Winter's Edge/II. The Protected Pages"},
+    [BASE + 59] = {"@Overworld/Winter's Edge/III. The Runaway Archeologist"},
+    [BASE + 60] = {"@Overworld/Winter's Edge/IV. The Treasure"},
     
     -- Final Rest
     [BASE + 67] = {"@Overworld/Final Rest/The Furbidden History"},
@@ -89,37 +89,37 @@ LOCATION_MAPPINGS = {
     [BASE + 5]  = {"@Overworld/Furbidden Fields/The Old Ruins"},
     
     -- Twin Towns - West
-    [BASE + 73] = {"@Overworld/Twin Towns - West/The Catnip Cure"},
-    [BASE + 75] = {"@Overworld/Twin Towns - West/The Knightmare"},
-    [BASE + 77] = {"@Overworld/Twin Towns - West/The Book"}, 
-    [BASE + 79] = {"@Overworld/Twin Towns - West/The Twin Resolution"},
+    [BASE + 73] = {"@Overworld/Twin Towns - West/I. The Catnip Cure"},
+    [BASE + 75] = {"@Overworld/Twin Towns - West/II. The Knightmare"},
+    [BASE + 77] = {"@Overworld/Twin Towns - West/III. The Book"}, 
+    [BASE + 79] = {"@Overworld/Twin Towns - West/IV. The Twin Resolution"},
     
     -- Twin Towns - East
-    [BASE + 74] = {"@Overworld/Twin Towns - East/The Catnip Cure...again"},
-    [BASE + 76] = {"@Overworld/Twin Towns - East/The Knightmare... again"},
-    [BASE + 78] = {"@Overworld/Twin Towns - East/The Book...again"},
+    [BASE + 74] = {"@Overworld/Twin Towns - East/I. The Catnip Cure...again"},
+    [BASE + 76] = {"@Overworld/Twin Towns - East/II. The Knightmare... again"},
+    [BASE + 78] = {"@Overworld/Twin Towns - East/III. The Book...again"},
     
     -- East Pawt
-    [BASE + 32] = {"@Overworld/East Pawt/The East Suspicion"},
-    [BASE + 33] = {"@Overworld/East Pawt/The East Shipment"},
-    [BASE + 34] = {"@Overworld/East Pawt/The East Escort"},
-    [BASE + 35] = {"@Overworld/East Pawt/The East Catfrontation"},
+    [BASE + 32] = {"@Overworld/East Pawt/I. The East Suspicion"},
+    [BASE + 33] = {"@Overworld/East Pawt/II. The East Shipment"},
+    [BASE + 34] = {"@Overworld/East Pawt/III. The East Escort"},
+    [BASE + 35] = {"@Overworld/East Pawt/IV. The East Catfrontation"},
     
     -- Old Masters Island
     [BASE + 8]  = {"@Overworld/Old Masters Island/The Old Friend"},
     
     -- Felingard Lake
-    [BASE + 7]  = {"@Overworld/Felingard Lake/Of Sea and Rock"},
+    [BASE + 7]  = {"@Overworld/Felingard Lake/Of Sea and Rock (1)"},
     
     -- Frozen Paw
     [BASE + 12] = {"@Overworld/Frozen Paw/Epilogue"},
     [BASE + 11] = {"@Overworld/Frozen Paw/The Dragons Void"},
     
     -- Windmew
-    [BASE + 53] = {"@Overworld/Windmew/The Flying Bush"},
-    [BASE + 54] = {"@Overworld/Windmew/The Magic Tree"},
-    [BASE + 55] = {"@Overworld/Windmew/The Cult"},
-    [BASE + 56] = {"@Overworld/Windmew/Dragonflight"},
+    [BASE + 53] = {"@Overworld/Windmew/I. The Flying Bush"},
+    [BASE + 54] = {"@Overworld/Windmew/II. The Magic Tree"},
+    [BASE + 55] = {"@Overworld/Windmew/III. The Cult"},
+    [BASE + 56] = {"@Overworld/Windmew/IV. Dragonflight"},
 
     -- Pawtato Farms
     [BASE + 70] = {"@Overworld/Pawtato Farms/The Pawtato Mystery"},
@@ -128,5 +128,5 @@ LOCATION_MAPPINGS = {
     [BASE + 72] = {"@Overworld/Death Isle/The Forgotten Hero"},
 
     -- Hilly Hills
-    [BASE + 6]  = {"@Overworld/Hilly Hills/Of Sea and Rock"},
+    [BASE + 6]  = {"@Overworld/Hilly Hills/Of Sea and Rock (2)"},
 }
