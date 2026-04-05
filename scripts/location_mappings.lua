b2bd413eb9f51f9c0d5e66c0f5c7f8e2315483f0
Apheayenne <78513012+Apheayenne@users.noreply.github.com>
2026-04-05 14:16:33 -0400

    Revert "Update to AP Version 0.3.0"

--- scripts/location_mappings.lua
+++ scripts/location_mappings.lua
@@ -1,114 +1,132 @@
-BASE = 0
---BASE = 123500
+BASE = 123500
 
 LOCATION_MAPPINGS = {
-
-    -- Main Quest
-    [BASE + 1] = {"@Overworld/Mewtown/A Whole New World"},
-    [BASE + 2] = {"@Overworld/Catpital - West/The Catpital (1)"},
-    [BASE + 3] = {"@Overworld/Catpital - West/The Catpital (2)"},
-    [BASE + 4] = {"@Overworld/Catpital Lake/The Dragon and the Cat"},
-    [BASE + 5] = {"@Overworld/Furbidden Fields/The Old Ruins"},
-    [BASE + 6] = {"@Overworld/Hilly Hills/Of Sea and Rock (1)"},
-    [BASE + 7] = {"@Overworld/Felingard Lake/Of Sea and Rock (2)"},
-    [BASE + 8] = {"@Overworld/Old Masters Island/The Old Friend"},
-    [BASE + 9] = {"@Overworld/South of Queens Resort/The Dragonblood"},
-    [BASE + 10] = {"@Overworld/Catpital Lake/The Dragonsbane"},
-    [BASE + 11] = {"@Overworld/Frozen Paw/The Dragons Void"},
-    [BASE + 12] = {"@Overworld/Frozen Paw/Epilogue"},
-
-    -- Side Quests
-    [BASE + 13] = {"@Overworld/Bermewda/I. The Strange Charm"},
-    [BASE + 14] = {"@Overworld/Bermewda/II. The Escape"},
-    [BASE + 15] = {"@Overworld/Bermewda/III. The Sacrifice"},
-    [BASE + 16] = {"@Overworld/Bermewda/IV. The Sanctuary"},
-    [BASE + 17] = {"@Overworld/Cat Grove/I. The Servant of God"},
-    [BASE + 18] = {"@Overworld/Cat Grove/II. The Offerings"},
-    [BASE + 19] = {"@Overworld/Cat Grove/III. The Prayer"},
-    [BASE + 20] = {"@Overworld/Cat Grove/IV. The Godcat, Mauth"},
-    [BASE + 21] = {"@Overworld/Queen's Resort/The First Day of Kitmas"},
-    [BASE + 22] = {"@Overworld/Fishercat's Friend/The Second Day of Kitmas"},
-    [BASE + 23] = {"@Overworld/Cat Grove/The Third Day of Kitmas"},
-    [BASE + 24] = {"@Overworld/West Pawt/The Fourth Day of Kitmas"},
-    [BASE + 25] = {"@Overworld/Queen's Resort/Mewry Kitmas!"},
-    [BASE + 26] = {"@Overworld/Catpital - East/I. The Missing Soldiers"},
-    [BASE + 27] = {"@Overworld/Catpital - East/II. The Rescue"},
-    [BASE + 28] = {"@Overworld/Catpital - West/I. The King's Mage"},
-    [BASE + 29] = {"@Overworld/Catpital - West/II. The Mage Search"},
-    [BASE + 30] = {"@Overworld/Catpital - West/III. The Spirits"},
-    [BASE + 31] = {"@Overworld/Catpital - West/IV. The Lion King"},
-    [BASE + 32] = {"@Overworld/East Pawt/I. The East Suspicion"},
-    [BASE + 33] = {"@Overworld/East Pawt/II. The East Shipment"},
-    [BASE + 34] = {"@Overworld/East Pawt/III. The East Escort"},
-    [BASE + 35] = {"@Overworld/East Pawt/IV. The East Catfrontation"},
-    [BASE + 36] = {"@Overworld/Fishercat's Friend/I. Meat Meat Meat!"},
-    [BASE + 37] = {"@Overworld/Fishercat's Friend/II. Meatmeatmeatmeat!!"},
-    [BASE + 38] = {"@Overworld/Fishercat's Friend/III. Meat Disposal Crew"},
-    [BASE + 39] = {"@Overworld/Fishercat's Friend/IV. The Meat Seller"},
+    -- Mewtown
+    [BASE + 1]  = {"@Overworld/Mewtown/A Whole New World"},
     [BASE + 40] = {"@Overworld/Mewtown/I. The Growling Peasant"},
     [BASE + 41] = {"@Overworld/Mewtown/II. The Catnip Ritual"},
     [BASE + 42] = {"@Overworld/Mewtown/III. The Circle"},
     [BASE + 43] = {"@Overworld/Mewtown/IV. Fur-reedom!"},
     [BASE + 44] = {"@Overworld/Mewtown/V. The Whispurrer"},
-    [BASE + 45] = {"@Overworld/Pawt City/I. Path to Water Walking"},
-    [BASE + 46] = {"@Overworld/Pawt City/II. The Fusion"},
-    [BASE + 47] = {"@Overworld/Pawt City/III. Rogue Mages"},
-    [BASE + 48] = {"@Overworld/Pawt City/IV. The Miracle"},
+
+    -- Catpital - West
+    [BASE + 2]  = {"@Overworld/Catpital - West/The Catpital (1)"},
+    [BASE + 28] = {"@Overworld/Catpital - West/I. The King's Mage"},
+    [BASE + 3]  = {"@Overworld/Catpital - West/The Catpital (2)"},
+    [BASE + 29] = {"@Overworld/Catpital - West/II. The Mage Search"},
+    [BASE + 30] = {"@Overworld/Catpital - West/III. The Spirits"},
+    [BASE + 31] = {"@Overworld/Catpital - West/IV. The Lion King"},
+
+    -- Catpital - East
+    [BASE + 26] = {"@Overworld/Catpital - East/I. The Missing Soldiers"},
+    [BASE + 27] = {"@Overworld/Catpital - East/II. The Rescue"},
+    [BASE + 65] = {"@Overworld/Catpital - East/The Dragon Worshippers"},
+    [BASE + 66] = {"@Overworld/Catpital - East/The Heirloom Armor"},
+
+    -- Catpital Lake
+    [BASE + 61] = {"@Overworld/Catpital Lake/I. Blacksmith Assistance"},
+    [BASE + 4]  = {"@Overworld/Catpital Lake/The Dragon and the Cat"},
+    [BASE + 62] = {"@Overworld/Catpital Lake/II. Blacksmith Apprentice"},
+    [BASE + 63] = {"@Overworld/Catpital Lake/III. Blacksmith Journeyman"},
+    [BASE + 64] = {"@Overworld/Catpital Lake/IV. Blacksmith Master"},
+    [BASE + 69] = {"@Overworld/Catpital Lake/The Ultimate Dragonsbane"},
+    [BASE + 10] = {"@Overworld/Catpital Lake/The Dragonsbane"},
+
+    -- Fishercat's Friend
+    [BASE + 36] = {"@Overworld/Fishercat's Friend/I. Meat Meat Meat!"},
+    [BASE + 37] = {"@Overworld/Fishercat's Friend/II. Meatmeatmeatmeat!!"},
+    [BASE + 38] = {"@Overworld/Fishercat's Friend/III. Meat Disposal Crew"},
+    [BASE + 39] = {"@Overworld/Fishercat's Friend/IV. The Meat Seller"},
+    [BASE + 22] = {"@Overworld/Fishercat's Friend/The Second Day of Kitmas"},
+    
+    -- Queen's Resort
+    [BASE + 21] = {"@Overworld/Queen's Resort/The First Day of Kitmas"},
+    [BASE + 71] = {"@Overworld/Queen's Resort/Everyone's invited!"},
+    [BASE + 25] = {"@Overworld/Queen's Resort/Mewry Kitmas!"},
+    
+    -- Bermewda
+    [BASE + 13] = {"@Overworld/Bermewda/I. The Strange Charm"},
+    [BASE + 14] = {"@Overworld/Bermewda/II. The Escape"},
+    [BASE + 15] = {"@Overworld/Bermewda/III. The Sacrifice"},
+    [BASE + 16] = {"@Overworld/Bermewda/IV. The Sanctuary"},
+    
+    -- Founders Island
+    [BASE + 68] = {"@Overworld/Founders Island/The Golden Key"},
+    
+    -- South of Queens Resort
+    [BASE + 9]  = {"@Overworld/South of Queens Resort/The Dragonblood"},
+    
+    -- Cat Grove
+    [BASE + 17] = {"@Overworld/Cat Grove/I. The Servant of God"},
+    [BASE + 18] = {"@Overworld/Cat Grove/II. The Offerings"},
+    [BASE + 23] = {"@Overworld/Cat Grove/The Third Day of Kitmas"},
+    [BASE + 19] = {"@Overworld/Cat Grove/III. The Prayer"},
+    [BASE + 20] = {"@Overworld/Cat Grove/IV. The Godcat, Mauth"},
+    
+    -- West Pawt
     [BASE + 49] = {"@Overworld/West Pawt/I. The West Investigation"},
     [BASE + 50] = {"@Overworld/West Pawt/II. The West Heist"},
     [BASE + 51] = {"@Overworld/West Pawt/III. The Magic Lock"},
     [BASE + 52] = {"@Overworld/West Pawt/IV. The Revelation"},
-    [BASE + 53] = {"@Overworld/Windmew/I. The Flying Bush"},
-    [BASE + 54] = {"@Overworld/Windmew/II. The Magic Tree"},
-    [BASE + 55] = {"@Overworld/Windmew/III. The Cult"},
-    [BASE + 56] = {"@Overworld/Windmew/IV. Dragonflight"},
+    [BASE + 24] = {"@Overworld/West Pawt/The Fourth Day of Kitmas"},
+    
+    -- Pawt City
+    [BASE + 45] = {"@Overworld/Pawt City/I. Path to Water Walking"},
+    [BASE + 46] = {"@Overworld/Pawt City/II. The Fusion"},
+    [BASE + 47] = {"@Overworld/Pawt City/III. Rogue Mages"},
+    [BASE + 48] = {"@Overworld/Pawt City/IV. The Miracle"},
+    
+    -- Winter's Edge
     [BASE + 57] = {"@Overworld/Winter's Edge/I. The Missing Pages"},
     [BASE + 58] = {"@Overworld/Winter's Edge/II. The Protected Pages"},
     [BASE + 59] = {"@Overworld/Winter's Edge/III. The Runaway Archeologist"},
     [BASE + 60] = {"@Overworld/Winter's Edge/IV. The Treasure"},
-    [BASE + 61] = {"@Overworld/Catpital Lake/I. Blacksmith Assistance"},
-    [BASE + 62] = {"@Overworld/Catpital Lake/II. Blacksmith Apprentice"},
-    [BASE + 63] = {"@Overworld/Catpital Lake/III. Blacksmith Journeyman"},
-    [BASE + 64] = {"@Overworld/Catpital Lake/IV. Blacksmith Master"},
-    [BASE + 65] = {"@Overworld/Catpital - East/The Dragon Worshippers"},
-    [BASE + 66] = {"@Overworld/Catpital - East/The Heirloom Armor"},
+    
+    -- Final Rest
     [BASE + 67] = {"@Overworld/Final Rest/The Furbidden History"},
-    [BASE + 68] = {"@Overworld/Founders Island/The Golden Key"},
-    [BASE + 69] = {"@Overworld/Catpital Lake/The Ultimate Dragonsbane"},
-    [BASE + 70] = {"@Overworld/Pawtato Farms/The Pawtato Mystery"},
-    [BASE + 71] = {"@Overworld/Queen's Resort/Everyone's invited!"},
-    [BASE + 72] = {"@Overworld/Death Isle/The Forgotten Hero"},
+    
+    -- Furbidden Fields
+    [BASE + 5]  = {"@Overworld/Furbidden Fields/The Old Ruins"},
+    
+    -- Twin Towns - West
     [BASE + 73] = {"@Overworld/Twin Towns - West/I. The Catnip Cure"},
-    [BASE + 74] = {"@Overworld/Twin Towns - East/I. The Catnip Cure...again"},
     [BASE + 75] = {"@Overworld/Twin Towns - West/II. The Knightmare"},
-    [BASE + 76] = {"@Overworld/Twin Towns - East/II. The Knightmare... again"},
-    [BASE + 77] = {"@Overworld/Twin Towns - West/III. The Book"},
-    [BASE + 78] = {"@Overworld/Twin Towns - East/III. The Book...again"},
+    [BASE + 77] = {"@Overworld/Twin Towns - West/III. The Book"}, 
     [BASE + 79] = {"@Overworld/Twin Towns - West/IV. The Twin Resolution"},
-     
-    --Spell  temples
-    [BASE + 80] = {"@Overworld/Catpital Temple/The Catpital Temple (Flamepurr)"},
-    [BASE + 81] = {"@Overworld/Bermewda Temple/Bermewda's Triangle Temple (Healing Paw)"},
-    [BASE + 82] = {"@Overworld/East Pawt Temple/East Pawt Temple (Lightnyan)"},
-    [BASE + 83] = {"@Overworld/Windmew Temple/Windmew City Temple (Cattrap)"},
-    [BASE + 84] = {"@Overworld/South Pawt Temple/South Pawt Temple (Purrserk)"},
-    [BASE + 85] = {"@Overworld/Death Isle Temple/Death Isle Temple (Astropaw)"},
-    [BASE + 86] = {"@Overworld/Felingard Lake Temple/Felingard Lake Temple (Freezepaw)"},
-     
-    --Story  Monuments
-    [BASE + 87] = {"@Overworld/Catpital Monument/Monument by The Catpital"},
-    [BASE + 88] = {"@Overworld/Puss Plains Monument/Monument in Puss Plains"},
-    [BASE + 89] = {"@Overworld/Furbidden Fields Monument/Monument below The Furbidden Fields"},
-    [BASE + 90] = {"@Overworld/South Point Monument/Monument at South Point"},
-    [BASE + 91] = {"@Overworld/East Pawt River Monument/Monument along East Pawt River"},
-    [BASE + 92] = {"@Overworld/Hilly Hills Monument/Monument by Hilly Hills Coast"},
-    [BASE + 93] = {"@Overworld/Unknown Head Monument/Monument by The Unknown Head"},
-    [BASE + 94] = {"@Overworld/Mountain Pass Monument/Monument above Mountain Pass"},
-    [BASE + 95] = {"@Overworld/Windmew Lake Monument/Monument by Windmew Lake"},
-    [BASE + 96] = {"@Overworld/North Point Monument/Monument at North Point"},
-    [BASE + 97] = {"@Overworld/Winter's Edge Monument/Monument by Winters Edge"},
-    [BASE + 98] = {"@Overworld/Old Master's Monument/Monument on Old Master's Island"},
-    [BASE + 99] = {"@Overworld/Key Island Monument/Monument on Key Island"},
-    [BASE + 100] = {"@Overworld/Monster Isle Monument/Monument on Monster Isle"},
-    [BASE + 101] = {"@Overworld/Death Isle Monument/Monument on Death Isle"}
+    
+    -- Twin Towns - East
+    [BASE + 74] = {"@Overworld/Twin Towns - East/I. The Catnip Cure...again"},
+    [BASE + 76] = {"@Overworld/Twin Towns - East/II. The Knightmare... again"},
+    [BASE + 78] = {"@Overworld/Twin Towns - East/III. The Book...again"},
+    
+    -- East Pawt
+    [BASE + 32] = {"@Overworld/East Pawt/I. The East Suspicion"},
+    [BASE + 33] = {"@Overworld/East Pawt/II. The East Shipment"},
+    [BASE + 34] = {"@Overworld/East Pawt/III. The East Escort"},
+    [BASE + 35] = {"@Overworld/East Pawt/IV. The East Catfrontation"},
+    
+    -- Old Masters Island
+    [BASE + 8]  = {"@Overworld/Old Masters Island/The Old Friend"},
+    
+    -- Felingard Lake
+    [BASE + 7]  = {"@Overworld/Felingard Lake/Of Sea and Rock (1)"},
+    
+    -- Frozen Paw
+    [BASE + 12] = {"@Overworld/Frozen Paw/Epilogue"},
+    [BASE + 11] = {"@Overworld/Frozen Paw/The Dragons Void"},
+    
+    -- Windmew
+    [BASE + 53] = {"@Overworld/Windmew/I. The Flying Bush"},
+    [BASE + 54] = {"@Overworld/Windmew/II. The Magic Tree"},
+    [BASE + 55] = {"@Overworld/Windmew/III. The Cult"},
+    [BASE + 56] = {"@Overworld/Windmew/IV. Dragonflight"},
+
+    -- Pawtato Farms
+    [BASE + 70] = {"@Overworld/Pawtato Farms/The Pawtato Mystery"},
+
+    -- Death Isle
+    [BASE + 72] = {"@Overworld/Death Isle/The Forgotten Hero"},
+
+    -- Hilly Hills
+    [BASE + 6]  = {"@Overworld/Hilly Hills/Of Sea and Rock (2)"},
 }
